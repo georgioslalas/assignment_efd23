a0fcee892af0356966bb8f35a66a5361ea75aab1
georgioslalas <125728791+georgioslalas@users.noreply.github.com>
2023-02-18 21:46:59 +0200

    Update part1_query2.sql

--- part1_query2.sql
+++ part1_query2.sql
@@ -1,33 +1,35 @@
 with top10 as (
-select
-city, 
-sum(y.orders_count) as top10users_orders_count
-FROM (
-select *
-from
-(
-     select city, 
+  select
+  city, 
+  sum(y.orders_count) as top10users_orders_count
+  from (
+     select *
+     from (
+            select
+            city, 
             user_id, 
             Row_number() over(partition by city order by count(*) desc) as ranking,
             count(*) as orders_count
-     from `efood2022-378208.main_assessment.orders`
-     group by city, 
-              user_id 
+            from `efood2022-378208.main_assessment.orders`
+            group by
+            city, 
+            user_id 
 
-) x
-where ranking <= 10
-) y
-group by city
+          ) x
+         where ranking <= 10
+  ) y
+  group by city
 )
+
 , cities as (
-select 
-city,
-count(*) as city_orders_count
-from `efood2022-378208.main_assessment.orders`
-group by city
+  select 
+  city,
+  count(*) as city_orders_count
+  from `efood2022-378208.main_assessment.orders`
+  group by city
 )
 
-select 
+SELECT 
 cities.city,
 cities.city_orders_count,
 top10.top10users_orders_count,
